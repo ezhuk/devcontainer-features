a43051f5892e0f5e9bcff4f79338c7a11aef7cb0
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2026-04-15 18:10:09 -0700

    Fix version variable in bazel
    Signed-off-by: Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>

--- src/bazel/install.sh
+++ src/bazel/install.sh
@@ -7,7 +7,7 @@ apt-get update
 apt-get install -y --no-install-recommends curl ca-certificates
 rm -rf /var/lib/apt/lists/*
 
-curl -fsSL -o /usr/local/bin/bazelisk "https://github.com/bazelbuild/bazelisk/releases/download/v{$VERSION}/bazelisk-linux-amd64"
+curl -fsSL -o /usr/local/bin/bazelisk "https://github.com/bazelbuild/bazelisk/releases/download/v${VERSION}/bazelisk-linux-amd64"
 
 chmod +x /usr/local/bin/bazelisk
 ln -sf /usr/local/bin/bazelisk /usr/local/bin/bazel
