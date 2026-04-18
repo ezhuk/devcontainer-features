78749d8ef579bb17fb7299e483428997677a58ad
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2026-04-17 17:43:57 -0700

    Add misc changes

--- src/uv/install.sh
+++ src/uv/install.sh
@@ -7,6 +7,9 @@ apt-get update
 apt-get install -y --no-install-recommends curl ca-certificates
 rm -rf /var/lib/apt/lists/*
 
-curl --proto '=https' --tlsv1.2 -LsSf "https://releases.astral.sh/github/uv/releases/download/${VERSION}/uv-installer.sh" | env UV_UNMANAGED_INSTALL=/usr/local/bin sh
+curl -fsSL -o uv.sh "https://github.com/astral-sh/uv/releases/download/${VERSION}/uv-installer.sh"
+
+UV_UNMANAGED_INSTALL=/usr/local/bin bash uv.sh
+rm uv.sh
 
 uv --version
