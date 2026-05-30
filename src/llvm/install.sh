38c22b3bf7eb432afdbfc707d6d608fcf4633ed7
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2026-05-29 17:37:21 -0700

    Add prefix

--- src/llvm/install.sh
+++ src/llvm/install.sh
@@ -2,7 +2,7 @@
 set -e
 
 VERSION="${VERSION:-22.1.6}"
-DIR="/opt/llvm/${VERSION}"
+ROOT="${ROOT:-/opt/llvm/${VERSION}}"
 
 apt-get update
 apt-get install -y --no-install-recommends curl ca-certificates xz-utils
@@ -10,14 +10,14 @@ rm -rf /var/lib/apt/lists/*
 
 curl -fsSL -o llvm.tar.xz "https://github.com/llvm/llvm-project/releases/download/llvmorg-${VERSION}/LLVM-${VERSION}-Linux-X64.tar.xz"
 
-mkdir -p "${DIR}"
-tar -xJf llvm.tar.xz -C "${DIR}" --strip-components=1
+mkdir -p "${ROOT}"
+tar -xJf llvm.tar.xz -C "${ROOT}" --strip-components=1
 rm llvm.tar.xz
 
-ln -sf "${DIR}/bin/clang" /usr/local/bin/clang
-ln -sf "${DIR}/bin/clang++" /usr/local/bin/clang++
+ln -sf "${ROOT}/bin/clang" /usr/local/bin/clang
+ln -sf "${ROOT}/bin/clang++" /usr/local/bin/clang++
 
-ln -sf "${DIR}/bin/clang-format" /usr/local/bin/clang-format
-ln -sf "${DIR}/bin/clang-tidy" /usr/local/bin/clang-tidy
+ln -sf "${ROOT}/bin/clang-format" /usr/local/bin/clang-format
+ln -sf "${ROOT}/bin/clang-tidy" /usr/local/bin/clang-tidy
 
 clang --version
