6f8b374dc00558bdab15efee207eced647864bd3
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2026-05-29 17:13:39 -0700

    Add root prefix
    Signed-off-by: Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>

--- src/bazel/install.sh
+++ src/bazel/install.sh
@@ -2,14 +2,15 @@
 set -e
 
 VERSION="${VERSION:-1.29.0}"
+ROOT="${ROOT:-/usr/local/bin}"
 
 apt-get update
 apt-get install -y --no-install-recommends curl ca-certificates
 rm -rf /var/lib/apt/lists/*
 
-curl -fsSL -o /usr/local/bin/bazelisk "https://github.com/bazelbuild/bazelisk/releases/download/v${VERSION}/bazelisk-linux-amd64"
+curl -fsSL -o "${ROOT}/bazelisk" "https://github.com/bazelbuild/bazelisk/releases/download/v${VERSION}/bazelisk-linux-amd64"
 
-chmod +x /usr/local/bin/bazelisk
-ln -sf /usr/local/bin/bazelisk /usr/local/bin/bazel
+chmod +x "${ROOT}/bazelisk"
+ln -sf "${ROOT}/bazelisk" "${ROOT}/bazel"
 
 bazel version
