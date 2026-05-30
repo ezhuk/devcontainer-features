50c1baadc6e2d0946a4e8b1c0f445755f150143e
Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>
2026-05-29 17:22:57 -0700

    Add prefix
    Signed-off-by: Eugene Zhuk <2105905+ezhuk@users.noreply.github.com>

--- src/gcc/install.sh
+++ src/gcc/install.sh
@@ -2,6 +2,7 @@
 set -e
 
 VERSION="${VERSION:-15.2.0-1}"
+ROOT="${ROOT:-/opt/gcc/${VERSION}}"
 
 apt-get update
 apt-get install -y --no-install-recommends curl ca-certificates
@@ -9,12 +10,12 @@ rm -rf /var/lib/apt/lists/*
 
 curl -fsSL -o gcc.tar.gz "https://github.com/xpack-dev-tools/gcc-xpack/releases/download/v${VERSION}/xpack-gcc-${VERSION}-linux-x64.tar.gz"
 
-mkdir -p "/opt/xpack-gcc/${VERSION}"
-tar -xzf gcc.tar.gz -C "/opt/xpack-gcc/${VERSION}" --strip-components=1
+mkdir -p "${ROOT}"
+tar -xzf gcc.tar.gz -C "${ROOT}" --strip-components=1
 rm gcc.tar.gz
 
-ln -sf "/opt/xpack-gcc/${VERSION}/bin/gcc" /usr/local/bin/gcc
-ln -sf "/opt/xpack-gcc/${VERSION}/bin/g++" /usr/local/bin/g++
+ln -sf "${ROOT}/bin/gcc" /usr/local/bin/gcc
+ln -sf "${ROOT}/bin/g++" /usr/local/bin/g++
 
 gcc --version
 g++ --version
